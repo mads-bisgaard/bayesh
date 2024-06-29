3bd96f959d9bbe8cbb5484a21bb320fd9fe93bcb
MadsBis <madsbisgaard88@hotmail.com>
2024-06-29 22:42:02 +0200

    add test for record_event

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,3 @@
-
 from bayesh._settings import BayeshSettings
 from bayesh._db import create_db, insert_row, _TABLE, Columns, Row, get_row, update_row
 import pytest
@@ -7,27 +6,32 @@ import sqlite3
 from faker import Faker
 from datetime import datetime
 
-def _get_n_rows(db: Path) -> int:
+
+def get_n_rows(db: Path) -> int:
     assert db.is_file()
     with sqlite3.connect(f"{db}") as conn:
         cursor = conn.cursor()
         cursor.execute(f"SELECT COUNT(*) FROM {_TABLE}")
         return cursor.fetchone()[0]
 
+
 def test_db_creation(tmp_path: Path):
-    db_file  = tmp_path / "my.db"
+    db_file = tmp_path / "my.db"
     create_db(db_file)
     assert db_file.is_file()
 
+
 def test_insert_row(tmp_path: Path, db: Path, faker: Faker):
     assert db.is_file()
-    assert _get_n_rows(db) == 0
+    assert get_n_rows(db) == 0
 
     previous_cmd = faker.text()
     current_cmd = faker.text()
     event_counter = faker.random_int(min=1, max=1000)
-    insert_row(db, Row(f"{tmp_path}", previous_cmd, current_cmd, event_counter, datetime.now()))
-    assert _get_n_rows(db) == 1
+    insert_row(
+        db, Row(f"{tmp_path}", previous_cmd, current_cmd, event_counter, datetime.now())
+    )
+    assert get_n_rows(db) == 1
 
     with sqlite3.connect(db) as conn:
         cursor = conn.cursor()
@@ -40,17 +44,30 @@ def test_insert_row(tmp_path: Path, db: Path, faker: Faker):
         row.current_cmd == current_cmd
         row.event_counter == event_counter
 
+
 def test_db_unique_key(tmp_path: Path, db: Path, faker: Faker):
     assert db.is_file()
-    assert _get_n_rows(db) == 0
+    assert get_n_rows(db) == 0
 
     previous_cmd = faker.text()
     current_cmd = faker.text()
     event_counter = faker.random_int(min=1, max=1000)
-    insert_row(db, Row(f"{tmp_path}", previous_cmd, current_cmd, event_counter, datetime.now()))
-    assert _get_n_rows(db) == 1
+    insert_row(
+        db, Row(f"{tmp_path}", previous_cmd, current_cmd, event_counter, datetime.now())
+    )
+    assert get_n_rows(db) == 1
     with pytest.raises(sqlite3.IntegrityError):
-        insert_row(db, Row(f"{tmp_path}", previous_cmd, current_cmd, faker.random_int(min=1, max=1000), datetime.now()))
+        insert_row(
+            db,
+            Row(
+                f"{tmp_path}",
+                previous_cmd,
+                current_cmd,
+                faker.random_int(min=1, max=1000),
+                datetime.now(),
+            ),
+        )
+
 
 def test_get_row(db: Path, faker: Faker, tmp_path: Path, row: Row):
     assert get_row(db, tmp_path, faker.text(), faker.text()) == None
@@ -62,13 +79,14 @@ def test_get_row(db: Path, faker: Faker, tmp_path: Path, row: Row):
     assert row.current_cmd == _row.current_cmd
     assert row.event_counter == _row.event_counter
 
+
 def test_update_row(db: Path, faker: Faker, row: Row):
-    assert _get_n_rows(db) == 0
+    assert get_n_rows(db) == 0
     insert_row(db, row)
 
     _event_counter = faker.random_int(min=1, max=1000)
     _last_modified = datetime.now()
     update_row(db, row, _event_counter, last_modified=_last_modified)
-    _row = get_row(db, row.cwd, row.previous_cmd, row.current_cmd)  
+    _row = get_row(db, row.cwd, row.previous_cmd, row.current_cmd)
     assert _row.event_counter == _event_counter
     assert _row.last_modified == f"{_last_modified}"
