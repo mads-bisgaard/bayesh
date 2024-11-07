ee5e8bb56f9b20386d9e0d9c4ec309df53e92f20
MadsBis <madsbisgaard88@hotmail.com>
2024-11-07 20:32:30 +0100

    start developing script

--- bayesh.bash
+++ bayesh.bash
@@ -3,19 +3,27 @@
 #set -e
 #set -u
 
+export BAYESH_DIR="${HOME}/Development/bayesh/tmp"
 
-function bayesh_record_event() {
-    bayesh record-event "${PWD}" "${BAYESH_CMD}" "$0"
+function bayesh_update() {
+    local cmd
+    cmd=$(fc -ln -1 | xargs)
+    bayesh record-event "${BAYESH_PWD}" "${BAYESH_CMD}" "${cmd}"
+
+
+    BAYESH_PWD=$(pwd)
+    export BAYESH_PWD
+    BAYESH_CMD=${cmd}
+    export BAYESH_CMD
 }
 
-export BAYESH_DIR="${HOME}/.bayesh"
-export BAYESH_CMD=""
-export PROMPT_COMMAND=
-#'$(bayesh record-event "${PWD}" "${BAYESH_CMD}" "$(fc -ln -1)") && export BAYESH_CMD=$(fc -ln -1)'
-
-
 if [[ -n "$PROMPT_COMMAND" ]]; then
-    PROMPT_COMMAND='$PROMPT_COMMAND; echo $(fc -ln -1)'
+    PROMPT_COMMAND="$PROMPT_COMMAND; bayesh_update"
 else
-    PROMPT_COMMAND='echo $(fc -ln -1)'
+    PROMPT_COMMAND='bayesh_update'
 fi
+
+BAYESH_PWD=$(pwd)
+export BAYESH_PWD
+BAYESH_CMD=""
+export BAYESH_CMD
