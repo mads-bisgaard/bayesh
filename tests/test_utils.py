4e8462d5bbfcce22728b2877221533548eb21158
MadsBis <madsbisgaard88@hotmail.com>
2024-12-07 08:43:20 +0100

    make test more readable

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 from bayesh._utils import sanitize_cmd
 from pathlib import Path
-from typing import Final, Generator
+from typing import Final, NamedTuple
 import csv
 import pytest
 
@@ -10,14 +10,17 @@ _COMMANDS_FILE: Final[Path] = (
 assert _COMMANDS_FILE.is_file()
 
 
+class CommandPair(NamedTuple):
+    raw_cmd: str
+    sanitized_cmd: str
+
+
 def _get_sanitized_commands() -> list[tuple[str, str]]:
     with open(_COMMANDS_FILE, mode="r") as f:
         csv_reader = csv.reader(f)
-        return [tuple(row) for row in csv_reader]
+        return [CommandPair(*row) for row in csv_reader]
 
 
-@pytest.mark.parametrize(
-    "cmd_expectedcmd", _get_sanitized_commands(), ids=lambda x: x[0]
-)
-def test_sanitize_cmd(cmd_expectedcmd: tuple[str, str]):
-    assert sanitize_cmd(cmd_expectedcmd[0]) == cmd_expectedcmd[1]
+@pytest.mark.parametrize("cmdpair", _get_sanitized_commands(), ids=lambda x: x[0])
+def test_sanitize_cmd(cmdpair: CommandPair):
+    assert sanitize_cmd(cmdpair.raw_cmd) == cmdpair.sanitized_cmd
