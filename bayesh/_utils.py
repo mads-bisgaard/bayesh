32e08ec3fcb186e8d00081b097989653907abc0a
MadsBis <madsbisgaard88@hotmail.com>
2024-12-08 11:53:48 +0100

    add bashlex dependency and start parsing bash commands

--- bayesh/_utils.py
+++ bayesh/_utils.py
@@ -1,2 +1,36 @@
+import re
+from typing import Final
+from bashlex import ast, parsesingle
+
+_PATH_REGEX: Final[str] = r"^(/[^/\0]+)+$"
+
+
+def _reconstruct_word_node(node: ast.node) -> str:
+    """process bashlex node of kind 'word'"""
+    assert hasattr(node, "kind") and node.kind == "word"  # nosec
+
+    if " " in node.word:
+        return f'"{node.word}"'
+    return node.word
+
+
+def _reconstruct_cmd_from_ast(cmd_ast: ast.node) -> str:
+    def walk_and_collect(node: ast.node):
+        parts = []
+        if hasattr(node, "parts") and len(node.parts) > 0:
+            for part in node.parts:
+                parts += walk_and_collect(part)
+        elif hasattr(node, "kind") and node.kind == "operator":
+            parts.append(node.op)
+        elif hasattr(node, "kind") and node.kind == "pipe":
+            parts.append(node.pipe)
+        elif hasattr(node, "kind") and node.kind == "word":
+            parts.append(_reconstruct_word_node(node))
+        return parts
+
+    return " ".join(walk_and_collect(cmd_ast))
+
+
 def sanitize_cmd(cmd: str) -> str:
+    parts = parsesingle(cmd)
     return cmd
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,9 @@
-from bayesh._utils import sanitize_cmd
+from bayesh._utils import sanitize_cmd, _reconstruct_cmd_from_ast
 from pathlib import Path
 from typing import Final, NamedTuple
 import csv
 import pytest
+import bashlex
 
 _COMMANDS_FILE: Final[Path] = (
     Path(__file__).parent / "data" / "sanitized_bash_commands.csv"
@@ -15,12 +16,25 @@ class CommandPair(NamedTuple):
     sanitized_cmd: str
 
 
-def _get_sanitized_commands() -> list[tuple[str, str]]:
+def _get_commands() -> list[tuple[str, str]]:
     with open(_COMMANDS_FILE, mode="r") as f:
-        csv_reader = csv.reader(f)
+        csv_reader = csv.reader(f, escapechar="\\")
         return [CommandPair(*row) for row in csv_reader]
 
 
-@pytest.mark.parametrize("cmdpair", _get_sanitized_commands(), ids=lambda x: x.raw_cmd)
+def _get_raw_commands() -> list[tuple[str, str]]:
+    return [cmdpair.raw_cmd for cmdpair in _get_commands()]
+
+
+@pytest.mark.parametrize(
+    "cmd", _get_raw_commands(), ids=lambda x: _get_raw_commands().index(x)
+)
+def test_reconstruct_cmd_from_ast(cmd):
+    assert _reconstruct_cmd_from_ast(bashlex.parsesingle(cmd)) == cmd
+
+
+@pytest.mark.parametrize(
+    "cmdpair", _get_commands(), ids=lambda x: _get_commands().index(x)
+)
 def test_sanitize_cmd(cmdpair: CommandPair):
     assert sanitize_cmd(cmdpair.raw_cmd) == cmdpair.sanitized_cmd
