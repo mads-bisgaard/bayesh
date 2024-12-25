6646af996d3d588444d8cc3bc604d2f7ee6b28a7
MadsBis <madsbisgaard88@hotmail.com>
2024-12-25 12:37:42 +0100

    add new command examples and test they work

--- bayesh/_command_processing.py
+++ bayesh/_command_processing.py
@@ -15,19 +15,19 @@ def process_cmd(cmd: str) -> str:
     parser = shlex.shlex(cmd, posix=True, punctuation_chars=True)
     parser.whitespace_split = True
     parts = list(parser)
-    for p in parts:
-        ii = parts.index(p)  # *first* index
-        if " " in p:
-            cmd = cmd.replace(f'"{p}"', "<MSG>")
-        elif (
+    for ii, p in enumerate(parts):
+        if cmd.count(p) > 1:
+            continue
+        if (
             Path(p).exists()
+            and p != "."
             and ii > 0
             and not parts[ii - 1].endswith(
                 ("(", ")", ";", "<", ">", "|", "&")
             )  # https://docs.python.org/3/library/shlex.html#improved-compatibility-with-shells
         ):  # allow paths in 0th position: pointing to executable
             cmd = cmd.replace(p, "<PATH>")
+        elif " " in p:
+            cmd = cmd.replace(f"{p}", "<MSG>")
 
-    cmd = re.sub(_MSG_REGEX_PATTERN, r"\1", cmd)
-    cmd = re.sub(_PATH_REGEX_PATTERN, r"\1", cmd)
     return cmd
