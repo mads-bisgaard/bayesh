4c5eeb56720d5d9f1833a2f80293baf15f32f3c4
MadsBis <madsbisgaard88@hotmail.com>
2025-02-27 22:51:57 +0100

    add shell integration and PATH addition directly in install script

--- install.sh
+++ install.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -uo pipefail
 
 REPO_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
 
 function _check_exists() {
-    local file_or_dir
-    file_or_dir=$1
-    if [[ ! -e "$file_or_dir" ]]; then
-        echo "- $file_or_dir does not exist"
-        exit 1
-    fi
+    [[ -e "$1" ]] || { echo "- $1 does not exist"; exit 1; }
 }
 
 function _check_dependency() {
-    command -v "$1" &> /dev/null || { echo "- Error: $1 is not installed." >&2; exit 1; }
+    command -v "$1" &> /dev/null || { echo "- Error: Required dependency $1 is not installed." >&2; exit 1; }
 }
 
+function allow() {
+  while true; do
+    read -rp "$1 ([y]/n) " answer
+    if [[ $answer = y ]]; then
+      return 0
+    elif [[ $answer = n ]]; then
+      return 1
+    fi
+  done
+}
 
 echo "- checking dependencies are installed"
 _check_dependency "python3"
@@ -28,16 +33,31 @@ _check_dependency "head"
 _check_dependency "tail"
 _check_dependency "echo"
 _check_dependency "grep"
+_check_dependency "curl"
+_check_dependency "jq"
 
 echo "- setting up python venv"
 python3 -m venv "${REPO_DIR}/.venv"
 _check_exists "${REPO_DIR}/.venv/bin/python3"
-echo "- installing bayesh into python venv"
+echo "- installing bayesh cli"
 "${REPO_DIR}/.venv/bin/python3" -m pip install "${REPO_DIR}" &> /dev/null
 _check_exists "${REPO_DIR}/.venv/bin/bayesh"
-_check_exists "/usr/local/bin"
-echo "- creating symlink in /usr/local/bin to expose bayesh executable on PATH"
-sudo ln -s "${REPO_DIR}/.venv/bin/bayesh" "${REPO_DIR}/bin/bayesh"
-_check_dependency "bayesh"
+echo "- adding bayesh executable to bin directory"
+[[ -e "${REPO_DIR}/bin/bayesh" ]] && rm "${REPO_DIR}/bin/bayesh"
+ln -s "${REPO_DIR}/.venv/bin/bayesh" "${REPO_DIR}/bin/bayesh"
 
-echo "- done installing bayesh"
+_shell=$(basename "$SHELL")
+[[ "$_shell" = "bash" ]] || [[ "$_shell" = "zsh" ]] || { echo "Currently Bayesh is only compatible with zsh and bash" >&2; exit 1; }
+_rcfile="$HOME/.${_shell}rc"
+
+if allow "- Add Bayesh to PATH (required for Bayesh to be functional)?"; then
+    # shellcheck disable=SC2016
+    echo 'export PATH="$PATH:'"${REPO_DIR}/bin"'"' >> "$_rcfile"
+fi
+
+if allow "- Add $_shell integration (required for Bayesh to be functional)?"; then
+    echo "source ${REPO_DIR}/shell/bayesh.${_shell}" >> "$_rcfile"
+fi
+
+echo "- done installing Bayesh. See https://github.com/mads-bisgaard/bayesh for documentation"
+exec "$_shell"
