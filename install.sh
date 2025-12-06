b2413bd089a5f50a8ecf85b050ce78858542dfda
Mads Rystok Bisgaard <madsbisgaard88@hotmail.com>
2025-12-06 12:46:41 +0100

    clean up install script

--- install.sh
+++ install.sh
@@ -4,22 +4,43 @@ set -e
 set -o pipefail
 
 version=v0.0.1
+_sudo="sudo"
+command -v sudo &> /dev/null || _sudo=""
 target_dir="/usr/local/bin"
 [ -d "$target_dir" ] || target_dir="/usr/bin"
 [ -d "$target_dir" ] || { echo "- Error: Could not find /usr/local/bin nor /usr/bin directories." >&2; exit 1; }
 
-_sudo="sudo"
-command -v sudo &> /dev/null || _sudo=""
+arch=$(uname -m)
+case "$arch" in
+    x86_64)
+        goarch="amd64"
+        ;;
+    armv7l|armv6l|arm)
+        goarch="arm"
+        ;;
+    *)
+        echo "Unsupported architecture: $arch. Please file an issue on https://github.com/mads-bisgaard/bayesh and I will add support for your architecture."
+        exit 1
+        ;;
+esac
 
-url_override=""
+function _usage() {
+    echo "Usage: install.sh [--help] [--url <url>]"
+    echo "Install Bayesh." 
+    echo "Options:"
+    echo "  --help         Show this help message and exit"
+    echo "  --url <url>    Overwrite the download URL"
+    exit 0
+}
 
+url="https://github.com/mads-bisgaard/bayesh/releases/download/${version}/bayesh-${version}-linux-${goarch}.tar.gz"
 while [[ $# -gt 0 ]]; do
     case $1 in
         --help)
             _usage
             ;;
         --url)
-            url_override="$2"
+            url="$2"
             shift
             shift
             ;;
@@ -31,27 +52,12 @@ while [[ $# -gt 0 ]]; do
 done
 
 
-function _usage() {
-    echo "Usage: $(basename "$0") [--help]"
-    echo "Install Bayesh." 
-    echo "Options:"
-    echo "  --help         Show this help message and exit"
-    echo "  --url <url>    Overwrite the download URL"
-    exit 0
-}
-
 function _check_dependency() {
     command -v "$1" &> /dev/null || { echo "- Error: Required dependency $1 is not installed." >&2; exit 1; }
 }
 
 function _install_bayesh(){
-    arc=$1
-    if [[ -n "$url_override" ]]; then
-        url="$url_override"
-    else
-        url="https://github.com/mads-bisgaard/bayesh/releases/download/${version}/bayesh-${version}-linux-${arc}.tar.gz"
-    fi
-    echo "- downloading Bayesh ${version} for architecture ${arc} to ${target_dir}/bayesh"
+    echo "- downloading Bayesh ${version} for architecture ${goarch} to ${target_dir}/bayesh"
     ${_sudo} curl -sSL "$url" | tar -xzf - -C "${target_dir}"
     ${_sudo} chmod +x "${target_dir}/bayesh"
     command -v "bayesh" &> /dev/null || { echo "- Error: bayesh could not be found after installation." >&2; exit 1; }
@@ -86,19 +92,5 @@ _check_dependency "grep"
 _check_dependency "curl"
 _check_dependency "jq"
 _check_dependency "tar"
-
-arch=$(uname -m)
-case "$arch" in
-    x86_64)
-        _install_bayesh "amd64"
-        ;;
-    armv7l|armv6l|arm)
-        _install_bayesh "arm"
-        ;;
-    *)
-        echo "Unsupported architecture: $arch. Please file an issue on https://github.com/mads-bisgaard/bayesh and I will add support for your architecture."
-        exit 1
-        ;;
-esac
-
+_install_bayesh
 _print_bayesh
