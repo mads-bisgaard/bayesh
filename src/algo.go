20c2be3422149a15b153a5dc1cb9f8034054215e
Mads Rystok Bisgaard <madsbisgaard88@hotmail.com>
2025-12-23 11:00:02 +0100

    cleanup of algo function

--- src/algo.go
+++ src/algo.go
@@ -9,16 +9,21 @@ import (
 
 const probabilityWeight float64 = (float64(1.0) / float64(3.0))
 
+type DatabaseQuerer interface {
+	ConditionalEventCounts(ctx context.Context, cwd *string, previousCmd *string, minRequiredEvents *int) (map[string]int, error)
+}
+
 func addConditionalProbabilities(
 	ctx context.Context,
 	settings *Settings,
-	queries *Queries,
+	queries DatabaseQuerer,
 	channel chan error,
 	result map[string]float64,
 	mu *sync.Mutex,
 	cwd *string,
 	processedPreviousCmd *string,
 ) {
+	slog.Debug("Adding conditional probability", *cwd, *processedPreviousCmd)
 
 	eventCounts, err := queries.ConditionalEventCounts(ctx, cwd, processedPreviousCmd, &settings.MinRequiredEvents)
 	if err != nil {
@@ -42,29 +47,37 @@ func addConditionalProbabilities(
 	channel <- nil
 }
 
-// ComputeCommandProbabilities computes a weighted average of conditional probabilities.
+// Computes a weighted average of conditional probabilities.
 // I.e. it computes the average of P(cmd|cwd), P(cmd|previousCmd), and P(cmd|cwd, previousCmd).
 // This can be thought of as a Bayesian inference (E(P(cmd|a)) where a is the context).
-func ComputeCommandProbabilities(ctx context.Context, settings *Settings, queries *Queries, cwd string, processedPreviousCmd string) (map[string]float64, error) {
-	nGoRoutines := 3
-	// Create a cancellable context to ensure resources are cleaned up if an error occurs
+func ComputeCommandProbabilities(ctx context.Context, settings *Settings, queries DatabaseQuerer, cwd string, processedPreviousCmd string) (map[string]float64, error) {
+	var mu sync.Mutex
+	var firstErr error
+
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	errCh := make(chan error, nGoRoutines)
+	inputs := []struct {
+		cwd *string
+		cmd *string
+	}{
+		{&cwd, nil},
+		{nil, &processedPreviousCmd},
+		{&cwd, &processedPreviousCmd},
+	}
+
+	errCh := make(chan error, len(inputs))
 	result := make(map[string]float64)
-	var mu sync.Mutex
 
-	go addConditionalProbabilities(ctx, settings, queries, errCh, result, &mu, &cwd, nil)
-	go addConditionalProbabilities(ctx, settings, queries, errCh, result, &mu, nil, &processedPreviousCmd)
-	go addConditionalProbabilities(ctx, settings, queries, errCh, result, &mu, &cwd, &processedPreviousCmd)
+	for _, input := range inputs {
+		go addConditionalProbabilities(ctx, settings, queries, errCh, result, &mu, input.cwd, input.cmd)
+	}
 
-	var firstErr error
-	for ii := 0; ii < nGoRoutines; ii++ {
+	for range len(inputs) {
 		if err := <-errCh; err != nil {
 			if firstErr == nil {
 				firstErr = err
-				cancel() // Cancel other goroutines immediately to save resources
+				cancel() // Cancel other goroutines on first error
 			}
 		}
 	}
